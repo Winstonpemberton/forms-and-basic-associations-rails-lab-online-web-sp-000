a49071284cc33c60266ed8b66e4724c05fa12005
Winston Pemberton <winstonpemberton@gmail.com>
2020-08-13 21:52:45 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,4 +3,12 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :Genre
   has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+def category_name
+   self.artist ? self.artist.name : nil
+end
 end
